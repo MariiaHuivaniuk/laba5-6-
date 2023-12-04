9a73326c329ecb34d5db29f51c99537317036a7b
Masha <huivaniuk.mariia@chnu.edu.ua>
2023-12-04 12:02:37 +0100

    Add project files.

---
+++ laba5(6)/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("length= ");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("width= ");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        double area = CalculateRectangleArea(length, width);
+        double volume = CalculateRectangleVolume(length, width);
+
+        Console.WriteLine($"Площа прямокутника: {area}");
+        Console.WriteLine($"Об'єм прямокутника: {volume}");
+    }
+
+    static double CalculateRectangleArea(double length, double width)
+    {
+        return length * width;
+    }
+
+    static double CalculateRectangleVolume(double length, double width)
+    {
+        
+        return CalculateRectangleArea(length, width) * width;
+    }
+}
